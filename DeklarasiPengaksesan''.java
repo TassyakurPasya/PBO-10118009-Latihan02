5462c5e45a37e828116c1fba28ee9ce10cbc013c
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-09-25 17:54:02 +0700

    Add files via upload

---
+++ DeklarasiPengaksesan''.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deklarasi.pengaksesan;
+
+/**
+ *
+ * @author Lenovo
+ * Nama : Tassyakur Pasya
+ * Kelas : IF-01
+ * NIM : 10118009
+ * Matkul : PBO-01
+ */
+public class DeklarasiPengaksesan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi Variabel 
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai variable 
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        // Manampilkan Hasil 
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = "+nilaiInt);
+        System.out.println("Isi Variabel PHI = "+PHI);
+        System.out.println("Isi Variabel Logika = "+nilaiLogika);
+        System.out.println("isi Variabel Karakter = "+nilaiKarakter);
+        
+    }
+    
+}
